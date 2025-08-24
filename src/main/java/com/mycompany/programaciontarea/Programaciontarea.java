3cb0b14b32b60df74d922d15bc5741a87818be1a
ortega2006 <lilicalderon280@gmail.com>
2025-08-23 18:27:27 -0600

    Publicación inicial del proyecto Java programaciontarea

---
+++ src/main/java/com/mycompany/programaciontarea/Programaciontarea.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.programaciontarea;
+
+/**
+ *
+ * @author Hogar
+ */
+public class Programaciontarea {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+   
+        int suma = 0;
+
+        for (int i = 1; i <= 10; i++) {
+            suma += i;
+        }
+
+        System.out.println("La suma de los primeros 10 numeros enteros positivos es: " + suma);
+    }
+}
